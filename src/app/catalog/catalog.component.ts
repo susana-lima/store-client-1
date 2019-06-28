98983162cc0b2d29474e5da84878b8737bf205df
Edson Terceros <edson.terceros@avantica.net>
2019-06-28 19:51:20 -0400

    Componentes Parte 1

--- src/app/catalog/catalog.component.ts
+++ src/app/catalog/catalog.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Item} from '../shared/item';
 
 @Component({
   selector: 'app-catalog',
@@ -7,7 +8,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CatalogComponent implements OnInit {
 
-  constructor() { }
+  items: Item[] = [
+    {
+      name: 'Tractor Kubota 10.5 Hp',
+      image: '/assets/images/tractorkubota.jpg',
+      category: 'machinery',
+      label: 'tractor',
+      price: '4.500',
+      description: 'Tractor útil para arado en superficies regulares sin pendientes pronunciadas.' +
+        ' Modelo JB11XA, Con Rotocultor, Diesel, 4x4, 10,5 HP. Año 2015'
+    },
+    {
+      name: 'Tractor kubota con pala 20Hp',
+      image: '/assets/images/tractorkubota-pala.jpg',
+      category: 'machinery',
+      label: 'tractor',
+      price: '10.500',
+      description: 'Tractor útil para arado en superficies con pendientes pronunciadas, para situaciones de alto esfuerzo. ' +
+        'Modelo L1-20DT, Con Rotocultor, Diesel 4x4, 20 HP, Con Pala.'
+    },
+    {
+      name: 'Motocultor yanmar 250',
+      image: '/assets/images/motocultor.jpg',
+      category: 'machinery',
+      label: 'motocultor',
+      price: '2.500',
+      description: 'Motocultor útil para arado en superficies de dificil acceso, con espacios reducidos, ' +
+        'para situaciones de alto esfuerzo. Arado De Mano Modelo YC80, Diesel.'
+    },
+    {
+      name: 'Montacarga',
+      image: '/assets/images/montacarga.jpg',
+      category: 'machinery',
+      label: '',
+      price: '15.999',
+      description: 'Hechos pensados en la industria donde la exigencia del equipo es frecuente. Komatsu BOBLE, ALTURA ELEVANTE 3,0 MTS'
+    }
+  ];
+
+  constructor() {
+  }
 
   ngOnInit() {
   }
