b3a87d741440ef62cb4758a5cb5270185d02bd14
Edson Terceros <edson.terceros@avantica.net>
2019-06-29 03:52:05 -0400

    Labo 4: subtarea 2 mostrar empleados en el componente about, adiconar navegaciones a about

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,4 +1,6 @@
 import {Component, OnInit} from '@angular/core';
+import {EmployeeService} from '../services/employee.service';
+import {Employee} from '../shared/employee';
 
 @Component({
   selector: 'app-about',
@@ -6,11 +8,13 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./about.component.scss']
 })
 export class AboutComponent implements OnInit {
+  employees: Employee[];
 
-  constructor() {
+  constructor(private employeeService: EmployeeService) {
   }
 
   ngOnInit() {
+    this.employees = this.employeeService.getEmployees();
   }
 
 }
--- src/app/app-routing/routes.ts
+++ src/app/app-routing/routes.ts
@@ -4,11 +4,13 @@ import {CatalogComponent} from '../catalog/catalog.component';
 import {HomeComponent} from '../home/home.component';
 import {ContactComponent} from '../contact/contact.component';
 import {ItemDetailComponent} from '../item-detail/item-detail.component';
+import {AboutComponent} from '../about/about.component';
 
 export const routes: Routes = [
   {path: 'home', component: HomeComponent},
   {path: 'catalogo', component: CatalogComponent},
   {path: 'contactanos', component: ContactComponent},
+  {path: 'acercade', component: AboutComponent},
   {path: 'itemdetail/:id', component: ItemDetailComponent},
   {path: '', redirectTo: '/home', pathMatch: 'full'}
 ];
