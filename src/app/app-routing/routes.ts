d922023ee09be65fd6a35bdaf7dbf3237e828fcc
Edson Terceros <edson.terceros@avantica.net>
2019-06-29 02:57:59 -0400

    contenido de contact component

--- src/app/app-routing/routes.ts
+++ src/app/app-routing/routes.ts
@@ -2,9 +2,11 @@ import {Routes} from '@angular/router';
 
 import {CatalogComponent} from '../catalog/catalog.component';
 import {HomeComponent} from '../home/home.component';
+import {ContactComponent} from '../contact/contact.component';
 
 export const routes: Routes = [
   {path: 'home', component: HomeComponent},
   {path: 'catalogo', component: CatalogComponent},
+  {path: 'contactanos', component: ContactComponent},
   {path: '', redirectTo: '/home', pathMatch: 'full'}
 ];
