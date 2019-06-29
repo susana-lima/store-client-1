0fe0705fb5ae32ced15bbf4a67685c6d10c858bd
Edson Terceros <edson.terceros@avantica.net>
2019-06-29 00:55:17 -0400

    extraer ITEMS a items.ts

--- src/app/catalog/catalog.component.ts
+++ src/app/catalog/catalog.component.ts
@@ -1,43 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {Item} from '../shared/item';
-
-const ITEMS: Item[] = [
-  {
-    name: 'Tractor Kubota 10.5 Hp',
-    image: '/assets/images/tractorkubota.jpg',
-    category: 'machinery',
-    label: 'tractor',
-    price: '4.500',
-    description: 'Tractor útil para arado en superficies regulares sin pendientes pronunciadas.' +
-      ' Modelo JB11XA, Con Rotocultor, Diesel, 4x4, 10,5 HP. Año 2015'
-  },
-  {
-    name: 'Tractor kubota con pala 20Hp',
-    image: '/assets/images/tractorkubota-pala.jpg',
-    category: 'machinery',
-    label: 'tractor',
-    price: '10.500',
-    description: 'Tractor útil para arado en superficies con pendientes pronunciadas, para situaciones de alto esfuerzo. ' +
-      'Modelo L1-20DT, Con Rotocultor, Diesel 4x4, 20 HP, Con Pala.'
-  },
-  {
-    name: 'Motocultor yanmar 250',
-    image: '/assets/images/motocultor.jpg',
-    category: 'machinery',
-    label: 'motocultor',
-    price: '2.500',
-    description: 'Motocultor útil para arado en superficies de dificil acceso, con espacios reducidos, ' +
-      'para situaciones de alto esfuerzo. Arado De Mano Modelo YC80, Diesel.'
-  },
-  {
-    name: 'Montacarga',
-    image: '/assets/images/montacarga.jpg',
-    category: 'machinery',
-    label: '',
-    price: '15.999',
-    description: 'Hechos pensados en la industria donde la exigencia del equipo es frecuente. Komatsu BOBLE, ALTURA ELEVANTE 3,0 MTS'
-  }
-];
+import {ITEMS} from '../shared/items';
 
 @Component({
   selector: 'app-catalog',
@@ -47,11 +10,16 @@ const ITEMS: Item[] = [
 export class CatalogComponent implements OnInit {
 
   items = ITEMS;
-  selectedItem = ITEMS[0];
+  selectedItem: Item;
+
   constructor() {
   }
 
   ngOnInit() {
+  }
+
+  onSelect(item: Item) {
+    this.selectedItem = item;
   }
 
 }
---
+++ src/app/shared/items.ts
@@ -0,0 +1,171 @@
+import {Item} from './item';
+
+export const ITEMS: Item[] = [
+  {
+    name: 'Tractor Kubota 10.5 Hp',
+    image: '/assets/images/tractorkubota.jpg',
+    category: 'machinery',
+    label: 'tractor',
+    price: '4.500',
+    description: 'Tractor útil para arado en superficies regulares sin pendientes pronunciadas.' +
+      ' Modelo JB11XA, Con Rotocultor, Diesel, 4x4, 10,5 HP. Año 2015',
+    comments: [
+      {
+        rating: 5,
+        comment: 'Imagina todo lo que puedes hacer con este tractor y su arado!',
+        author: 'Leo Messi',
+        date: '2012-10-16T17:57:28.556094Z'
+      },
+      {
+        rating: 4,
+        comment: 'Me dio mucho confort, desearia que mi suegro lo compre!',
+        author: 'Tania Barrancos',
+        date: '2014-09-05T17:57:28.556094Z'
+      },
+      {
+        rating: 3,
+        comment: 'Usalo, solamente usalo y veras que no hay comparacion con otros!',
+        author: 'Mario Rodriguez',
+        date: '2015-02-13T17:57:28.556094Z'
+      },
+      {
+        rating: 4,
+        comment: 'Lo ultimo, modelo de lujo!',
+        author: 'Damian Cardozo',
+        date: '2013-12-02T17:57:28.556094Z'
+      },
+      {
+        rating: 2,
+        comment: 'Simplemente es muy lindo!',
+        author: 'Gustavo Lopez',
+        date: '2011-12-02T17:57:28.556094Z'
+      }
+    ]
+  },
+  {
+    name: 'Tractor kubota con pala 20Hp',
+    image: '/assets/images/tractorkubota-pala.jpg',
+    category: 'machinery',
+    label: 'tractor',
+    price: '10.500',
+    description: 'Tractor útil para arado en superficies con pendientes pronunciadas, para situaciones de alto esfuerzo. ' +
+      'Modelo L1-20DT, Con Rotocultor, Diesel 4x4, 20 HP, Con Pala.'
+    ,
+    comments: [
+      {
+        rating: 5,
+        comment: 'Imagina todo lo qu puedes hacer con este tractor y su arado!',
+        author: 'Leo Messi',
+        date: '2012-10-16T17:57:28.556094Z'
+      },
+      {
+        rating: 4,
+        comment: 'Me dio mucho confort, desearia que mi suegro lo compre!',
+        author: 'Tania Barrancos',
+        date: '2014-09-05T17:57:28.556094Z'
+      },
+      {
+        rating: 3,
+        comment: 'UUsalo, solamente usalo y veras que no hay comparacion con otros!',
+        author: 'Mario Rodriguez',
+        date: '2015-02-13T17:57:28.556094Z'
+      },
+      {
+        rating: 4,
+        comment: 'Lo ultimo, modelo de lujo!',
+        author: 'Damian Cardozo',
+        date: '2013-12-02T17:57:28.556094Z'
+      },
+      {
+        rating: 2,
+        comment: 'Simplemente es muy lindo!',
+        author: 'Gustavo Lopez',
+        date: '2011-12-02T17:57:28.556094Z'
+      }
+    ]
+
+  },
+  {
+    name: 'Motocultor yanmar 250',
+    image: '/assets/images/motocultor.jpg',
+    category: 'machinery',
+    label: 'motocultor',
+    price: '2.500',
+    description: 'Motocultor útil para arado en superficies de dificil acceso, con espacios reducidos, ' +
+      'para situaciones de alto esfuerzo. Arado De Mano Modelo YC80, Diesel.'
+    ,
+    comments: [
+      {
+        rating: 5,
+        comment: 'Imagina todo lo qu puedes hacer con este tractor y su arado!',
+        author: 'Leo Messi',
+        date: '2012-10-16T17:57:28.556094Z'
+      },
+      {
+        rating: 4,
+        comment: 'Me dio mucho confort, desearia que mi suegro lo compre!',
+        author: 'Tania Barrancos',
+        date: '2014-09-05T17:57:28.556094Z'
+      },
+      {
+        rating: 3,
+        comment: 'UUsalo, solamente usalo y veras que no hay comparacion con otros!',
+        author: 'Mario Rodriguez',
+        date: '2015-02-13T17:57:28.556094Z'
+      },
+      {
+        rating: 4,
+        comment: 'Lo ultimo, modelo de lujo!',
+        author: 'Damian Cardozo',
+        date: '2013-12-02T17:57:28.556094Z'
+      },
+      {
+        rating: 2,
+        comment: 'Simplemente es muy lindo!',
+        author: 'Gustavo Lopez',
+        date: '2011-12-02T17:57:28.556094Z'
+      }
+    ]
+  },
+  {
+    name: 'Montacarga',
+    image: '/assets/images/montacarga.jpg',
+    category: 'machinery',
+    label: '',
+    price: '15.999',
+    description: 'Hechos pensados en la industria donde la exigencia del equipo es frecuente. Komatsu BOBLE, ALTURA ELEVANTE 3,0 MTS'
+    ,
+    comments: [
+      {
+        rating: 5,
+        comment: 'Imagina todo lo qu puedes hacer con este tractor y su arado!',
+        author: 'Leo Messi',
+        date: '2012-10-16T17:57:28.556094Z'
+      },
+      {
+        rating: 4,
+        comment: 'Me dio mucho confort, desearia que mi suegro lo compre!',
+        author: 'Tania Barrancos',
+        date: '2014-09-05T17:57:28.556094Z'
+      },
+      {
+        rating: 3,
+        comment: 'UUsalo, solamente usalo y veras que no hay comparacion con otros!',
+        author: 'Mario Rodriguez',
+        date: '2015-02-13T17:57:28.556094Z'
+      },
+      {
+        rating: 4,
+        comment: 'Lo ultimo, modelo de lujo!',
+        author: 'Damian Cardozo',
+        date: '2013-12-02T17:57:28.556094Z'
+      },
+      {
+        rating: 2,
+        comment: 'Simplemente es muy lindo!',
+        author: 'Gustavo Lopez',
+        date: '2011-12-02T17:57:28.556094Z'
+      }
+    ]
+  }
+];
