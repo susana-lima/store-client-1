f8eecf71d8eb45600ff00e415a496d7351a95145
Edson Terceros <edson.terceros@avantica.net>
2019-06-30 16:10:19 -0400

    observables a promesas

--- src/app/services/employee.service.ts
+++ src/app/services/employee.service.ts
@@ -1,6 +1,9 @@
 import {Injectable} from '@angular/core';
 import {Employee} from '../shared/employee';
 import {EMPLOYEES} from '../shared/employees';
+import {Observable} from 'rxjs';
+import 'rxjs-compat/add/observable/of';
+import 'rxjs-compat/add/operator/delay';
 
 @Injectable({
   providedIn: 'root'
@@ -11,14 +14,15 @@ export class EmployeeService {
   }
 
   getEmployees(): Promise<Employee[]> {
-    return Promise.resolve(EMPLOYEES);
+    return Observable.of(EMPLOYEES).delay(2000).toPromise();
+
   }
 
   getEmployee(id: number): Promise<Employee> {
-    return Promise.resolve(EMPLOYEES.filter(employee => (employee.id === id))[0]);
+    return Observable.of(EMPLOYEES.filter(employee => (employee.id === id))[0]).delay(2000).toPromise();
   }
 
   getFeaturedEmployee(): Promise<Employee> {
-    return Promise.resolve(EMPLOYEES.filter((employee) => employee.featured)[0]);
+    return Observable.of(EMPLOYEES.filter((employee) => employee.featured)[0]).delay(2000).toPromise();
   }
 }
--- src/app/services/item.service.ts
+++ src/app/services/item.service.ts
@@ -1,6 +1,9 @@
 import {Injectable} from '@angular/core';
 import {Item} from '../shared/item';
 import {ITEMS} from '../shared/items';
+import {Observable} from 'rxjs';
+import 'rxjs-compat/add/observable/of';
+import 'rxjs-compat/add/operator/delay';
 
 @Injectable({
   providedIn: 'root'
@@ -11,15 +14,15 @@ export class ItemService {
   }
 
   getItems(): Promise<Item[]> {
-    return Promise.resolve(ITEMS);
+    return Observable.of(ITEMS).delay(2000).toPromise();
   }
 
   getItem(id: number): Promise<Item> {
-    return Promise.resolve(ITEMS.filter((item) => (item.id === id))[0]);
+    return Observable.of(ITEMS.filter((item) => (item.id === id))[0]).delay(2000).toPromise();
   }
 
   getFeaturedItem(): Promise<Item> {
-    return Promise.resolve(ITEMS.filter((item) => item.featured)[0]);
+    return Observable.of(ITEMS.filter((item) => item.featured)[0]).delay(2000).toPromise();
   }
 
 }
