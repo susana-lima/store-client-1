63a61c1c08d10d7bceacdf82663e2df71acf0e47
Edson Terceros <edson.terceros@avantica.net>
2019-06-30 16:26:12 -0400

    RxJS Parte 2 Carrousell

--- src/app/item-detail/item-detail.component.ts
+++ src/app/item-detail/item-detail.component.ts
@@ -1,8 +1,9 @@
 import {Component, OnInit} from '@angular/core';
 import {Item} from '../shared/item';
-import {ActivatedRoute} from '@angular/router';
 import {ItemService} from '../services/item.service';
+import {ActivatedRoute, Params} from '@angular/router';
 import {Location} from '@angular/common';
+import 'rxjs/add/operator/switchMap';
 
 @Component({
   selector: 'app-item-detail',
@@ -12,20 +13,34 @@ import {Location} from '@angular/common';
 export class ItemDetailComponent implements OnInit {
 
   item: Item;
+  itemIds: number[];
+  prev: number;
+  next: number;
 
   constructor(private itemService: ItemService,
               private route: ActivatedRoute,
-              private location: Location
-  ) {
+              private location: Location) {
   }
 
   ngOnInit() {
-    const id = +this.route.snapshot.params.id;
-    this.itemService.getItem(id).subscribe(item => this.item = item);
+    this.itemService.getItemIds().subscribe(items => {
+      this.itemIds = items;
+      this.route.params
+        .switchMap((params: Params) => this.itemService.getItem(+params.id))
+        .subscribe(item => {
+          this.item = item;
+          this.setPrevNext(item.id);
+        });
+    });
   }
 
   goBack(): void {
     this.location.back();
   }
 
+  setPrevNext(itemId: number) {
+    const index = this.itemIds.indexOf(itemId);
+    this.prev = this.itemIds[(this.itemIds.length + index - 1) % this.itemIds.length];
+    this.next = this.itemIds[(this.itemIds.length + index + 1) % this.itemIds.length];
+  }
 }
--- src/app/services/item.service.ts
+++ src/app/services/item.service.ts
@@ -25,4 +25,7 @@ export class ItemService {
     return Observable.of(ITEMS.filter((item) => item.featured)[0]).delay(2000);
   }
 
+  getItemIds(): Observable<number[]> {
+    return Observable.of(ITEMS.map(item => item.id));
+  }
 }
